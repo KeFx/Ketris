6529c7295d4315118ef2686ccc4b1556b457cab9
Ke Hu <keywhobp@gmail.com>
2021-09-19 20:51:42 +1200

    #1 Refactor: Simpify function drawGamespace

--- Tetris.js
+++ Tetris.js
@@ -4,44 +4,45 @@
 const canvas = document.getElementById("Gameboard");
 const ctx = canvas.getContext("2d");
 
-drawGamespace(ctx);
+const GS_HEIGHT = 350;
+const GS_WIDTH = 200;
+const GS_BACKGROUND = "white";
 
-let pos = 100;
+drawGamespace(ctx, GS_HEIGHT, GS_WIDTH, GS_BACKGROUND);
+
+let pos = 0;
 let lastPos = pos;
 
 moveSquare()
+
 setInterval(function () {
     lastPos = pos;
-    pos += 15;
+    pos += 20;
     moveSquare();
-}, 800);
-
+}, 500);
 
 function moveSquare() {
-    drawSquare(ctx, (canvas.width) / 2, lastPos, 15, 50, "white");
+    drawSquare(ctx, (canvas.width) / 2, lastPos-5, 25, 50, "white");
     drawSquare(ctx, (canvas.width) / 2, pos, 50, 50, "lightblue");
 }
 
-function drawGamespace(c, height, width, background) {
-    height = height || 350;
-    width = width || 200;
-    background = background || 'white';
-
-    c.save();
-    c.beginPath();
-    c.fillStyle = background;
-
-    c.rect((canvas.width - width) / 2, (canvas.height - height) / 2, width, height);
-    c.fill();
-};
-
 function drawSquare(c, x, y, height, width, background) {
-    
     c.save();
     c.beginPath();
     c.fillStyle = background;
 
     c.rect(x, y, width, height);
     c.fill();
+    c.restore();
 }
+
+function drawGamespace(c, height, width , background) {
+    c.save();
+    c.beginPath();
+    c.fillStyle = background;
+
+    c.rect(0, 0, width, height);
+    c.fill();
+    c.restore();
+};
 
