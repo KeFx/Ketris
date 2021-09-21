ce78fb87833252e7a4c867c9016f1c1826450923
Ke Hu <keywhobp@gmail.com>
2021-09-21 21:22:36 +1200

    #1 more refactor: introduce new class 'square' and its methods 'display' and 'drop'

--- Tetris.js
+++ Tetris.js
@@ -9,7 +9,7 @@ const GS_WIDTH = canvas.width;
 const GS_BACKGROUND = "white";
 
 class Gameboard {
-    constructor(origin, context, bgColor, rows, cols, baseUnitSideLength){
+    constructor(origin, context, bgColor, rows, cols, baseUnitSideLength) {
         this.o = origin;
         this.c = context;
         this.bgColor = bgColor;
@@ -17,24 +17,56 @@ class Gameboard {
         this.cols = cols;
         this.baseUnitSideLength = baseUnitSideLength;
     }
-    drawGameboard(){
+    drawGameboard() {
         this.c.save();
         this.c.beginPath();
         this.c.fillStyle = this.bgColor;
 
-        this.c.rect(this.o.x, this.o.y, 
-                    this.cols * this.baseUnitSideLength,
-                    this.rows * this.baseUnitSideLength,);
+        this.c.rect(this.o.x, this.o.y,
+            this.cols * this.baseUnitSideLength,
+            this.rows * this.baseUnitSideLength);
         this.c.fill();
         this.c.restore();
     }
 
     startGame() {
-
+        const s1 = new Square(
+            { x: (this.cols) * this.baseUnitSideLength / 2, y: 0 },
+            this.c, this.baseUnitSideLength, "lightblue",
+            this.bgColor);
+        s1.display()
+        s1.drop();
     }
 
 }
+class Square {
+    constructor(origin, context, baseUnitSideLength, bgColor, eraseColor) {
+        this.o = origin;
+        this.c = context;
+        this.baseUnitSideLength = baseUnitSideLength;
+        this.bgColor = bgColor;
+        this.eraseColor = eraseColor;
+    }
+    display() {
+        drawRect(this.c, this.o.x, this.o.y,
+            this.baseUnitSideLength,
+            this.baseUnitSideLength,
+            this.bgColor)
+    }
+    drop() {
+        drawRect(this.c, this.o.x, this.o.y,
+            this.baseUnitSideLength,
+            this.baseUnitSideLength,
+            this.eraseColor)
 
+        this.o.y += this.baseUnitSideLength;
+
+        drawRect(this.c, this.o.x, this.o.y,
+            this.baseUnitSideLength,
+            this.baseUnitSideLength,
+            this.bgColor)
+    }
+}
 // const GB_ORIGIN = {x: 0, y: 0};
 // const GB_BACKGROUND_COLOR = "white";
 // const GB_ROW = 10
@@ -42,23 +74,24 @@ class Gameboard {
 // const BASE_UNIT
 
 
-const gb = new Gameboard({x:0, y:0}, ctx, "white", 40, 20, 10)
+const gb = new Gameboard({ x: 0, y: 0 }, ctx, "white", 40, 20, 10)
 
 gb.drawGameboard();
 gb.startGame();
 
-const BASE_SQUARE_SIDE_LENGTH = 10
 
-let pos = 0;
-let lastPos = pos;
-let x = canvas.width/2;
-let y = 0;
+// const BASE_SQUARE_SIDE_LENGTH = 10
 
-y = dropSquareByPixel(x, y, BASE_SQUARE_SIDE_LENGTH, BASE_SQUARE_SIDE_LENGTH);
+// let pos = 0;
+// let lastPos = pos;
+// let x = canvas.width / 2;
+// let y = 0;
 
-setInterval(function () {
-    y = dropSquareByPixel(x, y, BASE_SQUARE_SIDE_LENGTH, BASE_SQUARE_SIDE_LENGTH);
-}, 600);
+// y = dropSquareByPixel(x, y, BASE_SQUARE_SIDE_LENGTH, BASE_SQUARE_SIDE_LENGTH);
+
+// setInterval(function () {
+//     y = dropSquareByPixel(x, y, BASE_SQUARE_SIDE_LENGTH, BASE_SQUARE_SIDE_LENGTH);
+// }, 600);
 
 function dropSquareByPixel(x, y, size, dropPixel) {
     drawRect(ctx, x, y, dropPixel, size, "white");
@@ -77,7 +110,7 @@ function drawRect(c, x, y, height, width, background) {
     c.restore();
 }
 
-function drawGamespace(c, height, width , background) {
+function drawGamespace(c, height, width, background) {
     c.save();
     c.beginPath();
     c.fillStyle = background;
