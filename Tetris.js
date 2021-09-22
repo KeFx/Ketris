31e65fbdd595e39553ed4a69256b0ac4f11d2138
Ke Hu <keywhobp@gmail.com>
2021-09-22 21:30:07 +1200

    #2 Final refactor completion: introduces grid concept.

--- Tetris.js
+++ Tetris.js
@@ -12,7 +12,20 @@ class Gameboard {
         this.rows = rows;
         this.cols = cols;
         this.baseUnitSideLength = baseUnitSideLength;
+        this.grid = this.newEmptyGrid();
     }
+
+    newEmptyGrid() {
+        const g = new Array(this.rows);
+        for (let i = 0; i < g.length; i++) {
+            g[i] = new Array(this.cols)
+            for (let j = 0; j < g[i].length; j++) {
+                g[i][j] = false;
+            }
+        }
+        return g;
+    }
+
     drawGameboard() {
         this.c.save();
         this.c.beginPath();
@@ -25,21 +38,32 @@ class Gameboard {
         this.c.restore();
     }
 
+    gridToPixel(gridCoordinate) {
+        return { x: gridCoordinate.x * this.baseUnitSideLength, y: gridCoordinate.y * this.baseUnitSideLength }
+    }
+
     startGame() {
+        const currentSquarePos = { x: 4, y: 0 };
+
         const s1 = new Square(
-            { x: (this.cols) * this.baseUnitSideLength / 2, y: 0 },
-            this.c, this.baseUnitSideLength, "lightblue",this.bgColor);
+            this.gridToPixel(currentSquarePos),
+            this.c, this.baseUnitSideLength, "lightblue", this.bgColor);
 
         s1.display();
 
-        setInterval(function() {
-                        if ( !s1.hasCollide({x:0, y:315}) ){
-                            s1.drop();
-                        }
-                    },
-         600);
-    }
+        const rows = this.rows;
+        const grid = this.grid;
 
+        const gInterval = setInterval(function () {
+            if (currentSquarePos.y < rows - 1) {
+                s1.drop();
+                currentSquarePos.y++
+            } else {
+                grid[currentSquarePos.y][currentSquarePos.x] = true;
+            }
+        },
+            300);
+    }
 }
 
 class Square {
@@ -72,14 +96,12 @@ class Square {
             this.bgColor)
     }
 
-    hasCollide(obstacle) {
-        return (this.o.y + this.baseUnitSideLength === obstacle.y);
-    }
 }
 
 const gb = new Gameboard({ x: 0, y: 0 }, ctx, "white", 21, 10, 15)
 gb.drawGameboard();
 gb.startGame();
+// console.table(gb.grid);
 
 function drawRect(c, x, y, height, width, background) {
     c.save();
