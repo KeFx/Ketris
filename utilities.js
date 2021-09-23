afa9a5ab8b882c8e7927e45271b82288d2159c20
Ke Hu <keywhobp@gmail.com>
2021-09-23 16:55:30 +1200

    rename utilities.js to util.js

--- utilities.js
+++
@@ -1,9 +0,0 @@
-function drawRect(c, x, y, height, width, background) {
-    c.save();
-    c.beginPath();
-    c.fillStyle = background;
-
-    c.rect(x, y, width, height);
-    c.fill();
-    c.restore();
-}
