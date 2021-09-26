1b29e3108d5e379236fd196c111539cf09772c46
Ke Hu <keywhobp@gmail.com>
2021-09-26 16:11:21 +1300

    #9.2 Make IPiece extend Tetromino

--- I.js
+++ I.js
@@ -1,11 +1,6 @@
-class IPiece {
+class IPiece extends Tetromino {
     constructor(originHandlePoint, context, baseUnitSideLength, bgColor, eraseColor) {
-        this.handlePoint = originHandlePoint;
-        this.c = context;
-        this.baseUnitSideLength = baseUnitSideLength;
-        this.bgColor = bgColor;
-        this.eraseColor = eraseColor; 
-
+        super(originHandlePoint, context, baseUnitSideLength, bgColor, eraseColor);
     }
 
     fillShapeWithColor(color) {
@@ -13,14 +8,6 @@ class IPiece {
             this.baseUnitSideLength,
             this.baseUnitSideLength * 4,
             color)
-    }
-
-    eraseSelf() {
-        this.fillShapeWithColor(this.eraseColor);
-    }
-
-    display() {
-        this.fillShapeWithColor(this.bgColor);
     }
 
     returnCellsAfterDrop(handlePoint) {
@@ -50,32 +37,4 @@ class IPiece {
             {x: handlePoint.x + 2, y: handlePoint.y},{x: handlePoint.x + 3, y: handlePoint.y}
         ];
     }
-
-    moveTo(destCood) {
-        this.eraseSelf();
-        this.handlePoint = destCood;
-        this.display();
-    }
-
-    drop() {
-        this.moveTo({
-            x: this.handlePoint.x,
-            y: this.handlePoint.y + this.baseUnitSideLength
-        });
-    }
-
-    left() {
-        this.moveTo({
-            x: this.handlePoint.x - this.baseUnitSideLength,
-            y: this.handlePoint.y
-        });
-    }
-
-    right() {
-        this.moveTo({
-            x: this.handlePoint.x + this.baseUnitSideLength,
-            y: this.handlePoint.y
-        });
-    }
-
 }
---
+++ tetromino.js
@@ -0,0 +1,66 @@
+class Tetromino {
+    constructor(originHandlePoint, context, baseUnitSideLength, bgColor, eraseColor) {
+        this.handlePoint = originHandlePoint;
+        this.c = context;
+        this.baseUnitSideLength = baseUnitSideLength;
+        this.bgColor = bgColor;
+        this.eraseColor = eraseColor; 
+
+    }
+
+    fillShapeWithColor(color) {
+        throw new Error("Please override this function.");
+    }
+
+    eraseSelf() {
+        this.fillShapeWithColor(this.eraseColor);
+    }
+
+    display() {
+        this.fillShapeWithColor(this.bgColor);
+    }
+
+    returnCellsAfterDrop(handlePoint) {
+        throw new Error("Please override this function.");
+    }
+
+    returnCellsAfterMoveRight(handlePoint) {
+        throw new Error("Please override this function.");
+    }
+
+    returnCellsAfterMoveLeft(handlePoint) {
+        throw new Error("Please override this function.");
+    }
+
+    returnOccupiedCells(handlePoint) {
+        throw new Error("Please override this function.");
+    }
+
+    moveTo(destCood) {
+        this.eraseSelf();
+        this.handlePoint = destCood;
+        this.display();
+    }
+
+    drop() {
+        this.moveTo({
+            x: this.handlePoint.x,
+            y: this.handlePoint.y + this.baseUnitSideLength
+        });
+    }
+
+    left() {
+        this.moveTo({
+            x: this.handlePoint.x - this.baseUnitSideLength,
+            y: this.handlePoint.y
+        });
+    }
+
+    right() {
+        this.moveTo({
+            x: this.handlePoint.x + this.baseUnitSideLength,
+            y: this.handlePoint.y
+        });
+    }
+
+}
